47ca9aace9524647d563d559ff49b28893c0c921
Jemma <Jemma@jemsoftware.dev>
2023-04-26 13:18:06 -0500

    hardcoded test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const gpio = require('rpi-gpio').promise;
+const gpiop = require('rpi-gpio').promise;
 const app = express();
 const port = 8080;
 
@@ -10,11 +10,9 @@ app.use(express.static('public', {
 app.post('/v1/setpin/:value', (req, res) => {
     const num = Number(req.params.value);
     console.log(`Pin ${num} is on`);
-    gpio.setup(num, gpio.DIR_OUT)
+    gpiop.setup(7, gpiop.DIR_OUT)
         .then(() => {
-            gpio.write(num, true).then(() => {
-                return res.sendStatus(200);
-            })
+            return gpiop.write(7, true)
         })
         .catch((err) => {
             console.log('Error: ', err.toString())
@@ -24,11 +22,9 @@ app.post('/v1/setpin/:value', (req, res) => {
 app.delete('/v1/setpin/:value', (req, res) => {
     const num = Number(req.params.value);
     console.log(`Pin ${num} is off`);
-    gpio.setup(num, gpio.DIR_OUT)
+    gpiop.setup(7, gpiop.DIR_OUT)
         .then(() => {
-            gpio.write(num, false).then(() => {
-                return res.sendStatus(200);
-            })
+            return gpiop.write(7, false)
         })
         .catch((err) => {
             console.log('Error: ', err.toString())
